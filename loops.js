29d44d8ae5e0829c55bf8c7bdd941e5796dc86b4
Samuel Wilson <samwil56@gmail.com>
2021-02-12 21:39:33 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+let str = ()
+function addOne(strin) {
+  for(let i = 1; i<25; i +=1) {
+    if (i==1) {strin.push(`I am ${i} strange loop.`)}
+    else {strin.push(`I am ${i} strange loops.`)}
+  }
+  return strin
+}
+
+addOne(str)
