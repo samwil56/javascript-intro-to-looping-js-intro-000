3e8af7ac5d296abd4e9db09d7b1b9a981ff44a05
Samuel Wilson <samwil56@gmail.com>
2021-02-12 21:52:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 
 function forLoop(strin) {
-  for(let i = 1; i<25; i +=1) {
+  for(let i = 0; i<25; i +=1) {
     if (i==1) {strin.push(`I am ${i} strange loop.`)}
     else {strin.push(`I am ${i} strange loops.`)}
   }
